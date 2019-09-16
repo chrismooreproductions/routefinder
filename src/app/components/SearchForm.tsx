8c0373a6e36a264163fcc37a382f676aac291fce
Chris Moore <christopher.moore@mubaloo.com>
2019-09-16 10:53:56 +0100

    Added some refinement to the styling of the app. Converted most px values to rem

--- src/app/components/SearchForm.tsx
+++ src/app/components/SearchForm.tsx
@@ -14,11 +14,27 @@ type SearchFormProps = {
 
 const SearchForm: React.FC<SearchFormProps> = ({getRoutes, route, handleRouteChange}) => {
   return (
-    <form onSubmit={getRoutes} className="search-form">
-      <input type="text" className="search-form--input" name="routeStart" value={route.routeStart} onChange={handleRouteChange}></input>
-      <input type="text" className="search-form--input" name="routeEnd" value={route.routeEnd} onChange={handleRouteChange}></input>
-      <input type="submit" className="search-form--input" name="Submit"></input>
-    </form>
+    <section className="search-form">
+      <h1 className="search-form--title">Welcome to Routefinder!</h1>
+      <h2 className="search-form--sub-title">To start please enter a start and end point for your route.</h2>
+      <form onSubmit={getRoutes} className="search-form--wrapper">
+        <div className="search-form--input">
+          <label htmlFor="routeStart">
+            Route Start Point:
+          </label>
+          <input type="text" className="search-form--input-field" name="routeStart" value={route.routeStart} onChange={handleRouteChange}></input>
+        </div>
+        <div className="search-form--input">
+          <label htmlFor="routeEnd">
+            Route End Point: 
+          </label>
+          <input type="text" className="search-form--input-field" name="routeEnd" value={route.routeEnd} onChange={handleRouteChange}></input>
+        </div>
+        <div className="search-form--submit">
+          <input type="submit" className="search-form--submit-button" name="Submit"></input>
+        </div>
+      </form>
+    </section>
   )
 }
 
